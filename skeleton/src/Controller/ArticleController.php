f833fb6d8a386fd1355e2004c388c7d40c607f3c
darkoni59 <darkoni59610@gmail.com>
2019-02-13 13:17:34 +0100

    instal sympho

--- skeleton/src/Controller/ArticleController.php
+++ skeleton/src/Controller/ArticleController.php
@@ -12,11 +12,21 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController
 {
+    /**
+     * @return Response
+     * @route("/")
+     */
     public function homepage() {
         return new Response('test');
+    }
+    /** @route("/article/mon-super-article") */
+    function show(){
+        return new  Response("Mon super article s'affiche");
+
     }
 
 }
